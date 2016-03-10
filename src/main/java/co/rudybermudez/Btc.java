1e28044a541053ae88343f2a1cee38edfe704cbe
Rudy Bermudez <hello@rudybermudez.co>
2016-03-10 16:13:11 +0100

    add class Btc
    Btc makes a GET request to Coinbase and returns the current price of Bitcoin as a formatted String.

---
+++ src/main/java/co/rudybermudez/Btc.java
@@ -0,0 +1,19 @@
+package co.rudybermudez;
+
+/*
+ * @Author:  Rudy Bermudez
+ * @Email:   hello@rudybermudez.co
+ * @Date:    Mar 10, 2016
+ * @Project: SmartAlarm
+ * @Package:    co.rudybermudez
+ */
+
+public class Btc {
+
+    public static String getCurrentPrice() throws Exception {
+        String url = "https://api.exchange.coinbase.com/products/BTC-USD/ticker";
+        JsonEngine jsonEngine = new JsonEngine(JsonEngine.makeConnection(url));
+        return String.format("The current price of bitcoin is $%s.", jsonEngine.parseSimple("price").toString());
+    }
+
+}
