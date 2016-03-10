223b8358f286e7870d0aee6e16ddd3dd35846be7
Rudy Bermudez <hello@rudybermudez.co>
2016-03-10 15:39:22 +0100

    Initial Commit
    I will be developing a smart alarm clock using the dependencies OkHttp, Ivona, and jlayer.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/*
+ * @Author:  Rudy Bermudez
+ * @Email:   hello@rudybermudez.co
+ * @Date:    Mar 10, 2016
+ * @Project: SmartAlarm
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
