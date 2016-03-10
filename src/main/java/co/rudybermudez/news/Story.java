9d929f8e4534645247d1bb00bfa0eb190362fd53
Rudy Bermudez <hello@rudybermudez.co>
2016-03-10 16:29:23 +0100

    Add class Story
    Story is a model class that will store the title and text of a news story. This class will be the foundation of getting news stories from various sources.

---
+++ src/main/java/co/rudybermudez/news/Story.java
@@ -0,0 +1,33 @@
+package co.rudybermudez.news;
+
+/*
+ * @Author:  Rudy Bermudez
+ * @Email:   hello@rudybermudez.co
+ * @Date:    Mar 10, 2016
+ * @Project: SmartAlarm
+ * @Package:    co.rudybermudez.news
+ */
+
+public class Story {
+
+    private String mTitle;
+    private String mAbstract;
+
+    public Story(String title, String anAbstract) {
+        mTitle = title;
+        mAbstract = anAbstract;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAbstract() {
+        return mAbstract;
+    }
+
+    @Override
+    public String toString() {
+        return mTitle + ".\n" + mAbstract + "\n\n";
+    }
+}
