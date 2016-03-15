b424736a2618aa81ba37752ec996fd022e95bf1e
Rudy Bermudez <hello@rudybermudez.co>
2016-03-15 14:32:25 +0100

    Optimize Code
    I removed the giant set of If statements in the method getWindConditions and replaced it with 2 TreeMaps. One for Metric and one for Imperial. There is a for loop that compares the current wind speed to the set of intervals in the TreeMap and determines the proper message accordingly.

--- src/main/java/co/rudybermudez/weather/Weather.java
+++ src/main/java/co/rudybermudez/weather/Weather.java
@@ -14,6 +14,7 @@ import com.amazonaws.util.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TreeMap;
 
 public class Weather {
 
@@ -55,87 +56,30 @@ public class Weather {
         return null;
     }
 
-
     private String getWindConditions(Integer windSpeed, Integer windChill) {
         String windReport = "";
         String unitSpeed = mLocation.getUnits().unitSpeed;
         String unitTemp = mLocation.getUnits().unitTemperature;
+        TreeMap<Integer,String> windSpeedMap = mLocation.getUnits().windSpeedMap;
         Integer month = Integer.parseInt(new SimpleDateFormat("L").format(new Date()));
         String windCondition = "";
         String wind_chill;
 
-        // TODO:rmb 3/10/16 Create enum. Enum stores HashSet. HashSet for each unit. Create for loop.
-        // If windSpeed > preset value, continue. When it is so longer larger, stop and get message.
-
-        if (mLocation.getUnits().unitSpeed.equals("MPH")) {
-            if (!windSpeed.toString().isEmpty()) {
-                if (windSpeed < 1)
-                    windCondition = "The wind is calm.";
-                if (windSpeed > 1)
-                    windCondition = "There is currently a Light Zephyr.";
-                if (windSpeed > 3)
-                    windCondition = String.format("There is currently a light breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 7)
-                    windCondition = String.format("There is currently a gentle breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 12)
-                    windCondition = String.format("There is currently a moderate breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 18)
-                    windCondition = String.format("There is currently a strong breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 24)
-                    windCondition = String.format("There is currently a strong wind of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 31)
-                    windCondition = String.format("There are currently high winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 38)
-                    windCondition = String.format("There are currently Gale force winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 46)
-                    windCondition = String.format("There is currently a strong gale at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 55)
-                    windCondition = String.format("There are currently Storm winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 64)
-                    windCondition = String.format("There are currently Violent storm winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 73)
-                    windCondition = String.format("There are curerntly Hurricane force winds at %s %s. ", windSpeed, unitSpeed);
-            }
-        } else {
-            if (!windSpeed.toString().isEmpty()) {
-                if (windSpeed < 1)
-                    windCondition = "The wind is calm.";
-                if (windSpeed > 1)
-                    windCondition = "There is currently a Light Zephyr.";
-                if (windSpeed > 5)
-                    windCondition = String.format("There is currently a light breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 11)
-                    windCondition = String.format("There is currently a gentle breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 19)
-                    windCondition = String.format("There is currently a moderate breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 28)
-                    windCondition = String.format("There is currently a strong breeze of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 39)
-                    windCondition = String.format("There is currently a strong wind of %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 50)
-                    windCondition = String.format("There are currently high winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 61)
-                    windCondition = String.format("There are currently Gale force winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 74)
-                    windCondition = String.format("There is currently a strong gale at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 88)
-                    windCondition = String.format("There are currently Storm winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 102)
-                    windCondition = String.format("There are currently Violent storm winds at %s %s. ", windSpeed, unitSpeed);
-                if (windSpeed > 117)
-                    windCondition = String.format("There are curerntly Hurricane force winds at %s %s. ", windSpeed, unitSpeed);
-            }
+        for (Integer speedInterval : windSpeedMap.keySet()) {
+            if (windSpeed >= speedInterval)
+                windCondition = String.format(windSpeedMap.get(speedInterval),windSpeed,unitSpeed);
 
         }
         if (windSpeed > 5 && (month < 4 || month > 10)) {
             wind_chill = String.format("With the windchill it feels like %s degrees %s", windChill, unitTemp);
             windReport = windCondition + wind_chill + ".";
+        } else {
+            windReport = windCondition;
         }
 
 
         return windReport;
     }
-
 
     public String getCurrentWeather() {
         Forecast currentForecast = parseForecastJSON(mLocation.getJsonData(), 0);
@@ -168,5 +112,38 @@ public class Weather {
         return weather;
     }
 
+    public static TreeMap<Integer,String> ImperialWindMap(){
+        TreeMap<Integer, String> windSpeedMap = new TreeMap<>();
+        windSpeedMap.put(1, "There is currently a Light Zephyr of %s %s. ");
+        windSpeedMap.put(3, "There is currently a light breeze of %s %s. ");
+        windSpeedMap.put(7, "There is currently a gentle breeze of %s %s. ");
+        windSpeedMap.put(12, "There is currently a moderate breeze of %s %s. ");
+        windSpeedMap.put(18, "There is currently a strong breeze of %s %s. ");
+        windSpeedMap.put(24, "There is currently a strong wind of %s %s. ");
+        windSpeedMap.put(31, "There are currently high winds at %s %s. ");
+        windSpeedMap.put(38, "There are currently Gale force winds at %s %s. ");
+        windSpeedMap.put(46, "There is currently a strong gale at %s %s. ");
+        windSpeedMap.put(55, "There are currently Storm winds at %s %s. ");
+        windSpeedMap.put(64, "There are currently Violent storm winds at %s %s. ");
+        windSpeedMap.put(73, "There are currently Hurricane force winds at %s %s. ");
+        return windSpeedMap;
+    }
+
+    public static TreeMap<Integer,String> MetricWindMap(){
+        TreeMap<Integer, String> windSpeedMap = new TreeMap<>();
+        windSpeedMap.put(1, "There is currently a Light Zephyr of %s %s. ");
+        windSpeedMap.put(5, "There is currently a light breeze of %s %s. ");
+        windSpeedMap.put(11, "There is currently a gentle breeze of %s %s. ");
+        windSpeedMap.put(19, "There is currently a moderate breeze of %s %s. ");
+        windSpeedMap.put(28, "There is currently a strong breeze of %s %s. ");
+        windSpeedMap.put(39, "There is currently a strong wind of %s %s. ");
+        windSpeedMap.put(50, "There are currently high winds at %s %s. ");
+        windSpeedMap.put(61, "There are currently Gale force winds at %s %s. ");
+        windSpeedMap.put(74, "There is currently a strong gale at %s %s. ");
+        windSpeedMap.put(88, "There are currently Storm winds at %s %s. ");
+        windSpeedMap.put(102, "There are currently Violent storm winds at %s %s. ");
+        windSpeedMap.put(117, "There are currently Hurricane force winds at %s %s. ");
+        return windSpeedMap;
+    }
 
 }
